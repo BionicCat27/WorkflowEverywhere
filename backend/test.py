91db23fc2613b7260c202aa3da8069da97911f01
Aslan Bennington-Barr <aslan.bennington-barr@techtime.co.nz>
2023-12-07 23:48:34 +1300

    Correct PUT /shortcuts test

--- backend/test.py
+++ backend/test.py
@@ -7,10 +7,16 @@ def test():
     print("Doing healthcheck")
     result = requests.get(f"{HOST}/healthcheck")
     assert result.status_code == 200
+
+    print("Clearing config")
+    result = requests.put(f"{HOST}/shortcuts", json=[])
+    assert result.status_code == 200
     
     print("Doing GET shortcuts")
     result = requests.get(f"{HOST}/shortcuts")
-    content = json.loads(result.content)
+    content = result.content
+    if content != "":
+        content = json.loads(content)
     assert result.status_code == 200
     assert content == []
 
@@ -23,14 +29,21 @@ def test():
     result = requests.put(f"{HOST}/shortcuts", json=data)
     assert result.status_code == 200
     result = requests.get(f"{HOST}/shortcuts")
-    content = json.loads(result.content)
-    print(content)
     assert result.status_code == 200
+    content = json.loads(result.content)
     assert content == data
-    
-    
 
-    
+    print("Doing POST shortcut")
+    data = {
+        "test2": "test"
+    }
+    result = requests.post(f"{HOST}/shortcuts/add", json=data)
+    assert result.status_code == 200
+    result = requests.get(f"{HOST}/shortcuts")
+    assert result.status_code == 200
+    content = json.loads(result.content)
+    assert len(content) == 2
+
     print("Finished tests")
 
 if __name__ == "__main__":
